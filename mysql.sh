7f392132735fa15c860bdd86022c643602b74d1e
siri-ponnapalli <ponnapallisirichandana@gmail.com>
2024-09-03 18:46:46 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -45,13 +45,27 @@ VALIDATE $? "Enabled MySQL Server"
 systemctl start mysqld &>>$LOG_FILE
 VALIDATE $? "Started MySQL Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
-VALIDATE $? "Setting up root password"
+mysql -h mysql.mydevops.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE  
+if [ $? -ne 0 ]
+then
+    echo "Mysql root password is not setup, setting now" &>>$LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "setting up root password"
+else 
+    echo "Mysql root password is already setup... $Y SKIPPING $N" | tee &>>$LOG_FILE
+fi    
 
 
 
 
 
+
+
+
+
+
+
+ 
 
 
 
